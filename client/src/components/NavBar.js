abfb1cbd2179f43d402d49490336781991e01657
Naomii426 <124053897+Naomii426@users.noreply.github.com>
2023-01-31 18:06:30 +0300

    Added authorization check, wrapped NavBar in observer function

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,21 +1,28 @@
 import React, {useContext} from 'react';
 import {Context} from "../index";
 import {Button, Container, Nav, Navbar, NavLink} from "react-bootstrap";
-import Shop from "../pages/Shop";
 import {SHOP_ROUTES} from "../utils/consts";
+import {observer} from "mobx-react-lite";
 
-const NavBar = () => {
+const NavBar = observer(() => {             //обернем весь блок в функцию observer, для того чтобы mobx мог отслеживать изменение состояний и перерендеривать объект
     const {user} = useContext(Context) //добавляем контекст, потому что в зависимости от авторизованности NavBar будет отображаться по разному
     return (
         <Navbar bg="dark" variant="dark">
             <Container>
-                <NavLink style={{color:'white'}}to={SHOP_ROUTES}>LumusLamp</NavLink>
+                <NavLink style={{color:'white'}} to={SHOP_ROUTES}>LumusLamp</NavLink>
+                {user.isAuth ?                                                      //Если пользователь авторизован, то NavBar будет отображаться так
                 <Nav className="ml-auto" style={{color: 'white'}}>
-                    <Button variant={"outline-secondary"}>Авторизация</Button>
+                    <Button variant={"outline-secondary"}>Панель адинистратора</Button>
+                    <Button variant={"outline-secondary"}>Выйти</Button>
                 </Nav>
+                    :                                                               //Если пользователь неавторизован, то NavBar будет отображаться так
+                    <Nav className="ml-auto" style={{color: 'white'}}>
+                        <Button variant={"outline-secondary"} onClick={() => user.setIsAuth(true)}>Авторизация</Button>
+                    </Nav>
+                }
             </Container>
         </Navbar>
     );
-};
+});
 
 export default NavBar;
